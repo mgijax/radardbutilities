7d516b669d7f84cb08403b537668c0c523159a9f
Lori Corbani <Lori.Corbani@jax.org>
2009-02-10 15:48:50 +0000

    remove

--- bin/logFileToProcess.py
+++ bin/logFileToProcess.py
@@ -3,20 +3,20 @@
 #
 # Program:
 #
-#    logFileToProcess.py
+#    logPreMirroredFilesByFile.py
 #
 # Original Author:
 #
-#    sc
+#    Lori Corbani
 #
 # Purpose:
 #
-#    Log a file into APP_FilesMirrored table in RADAR
+#    Log pre-processed mirrored files into APP_FilesMirrored table in RADAR
 #
 # Requirements Satisfied by This Program:
 #
 # Usage:
-#	logFileToProcess.py
+#	logPreMirroredFilesByFile.py
 #
 # Envvars:
 #
@@ -24,7 +24,8 @@
 #    RADAR_DBNAME
 #    RADAR_DBUSER
 #    RADAR_DBPASSWORDFILE
-#    FILETOLOG
+#    LOGWORKFILE
+#    LOGOUTPUTDIR
 #    LOGFILETYPE
 #    USER (unix login)
 #
@@ -41,6 +42,7 @@
 #    0 if files were successfully logged
 #    1 if file logging failed
 #
+#    Note:  If there were no files to log, then 0 is returned
 #
 # Assumes:
 #
@@ -52,8 +54,8 @@
 #
 # Modification History:
 #
-# 11/05/2007 
-#	-  created
+# 10/30/2008 lec
+#	- TR9050
 #
 
 import sys
@@ -63,49 +65,45 @@ import time
 import db
 import mgi_utils
 
-# current date
 cdate = mgi_utils.date('%m/%d/%Y')
 
 #
 # Main
 #
 
-# get database settings from the environment
 server = os.environ['RADAR_DBSERVER']
 database = os.environ['RADAR_DBNAME']
 user = os.environ['RADAR_DBUSER']
 passwordFileName = os.environ['RADAR_DBPASSWORDFILE']
 password = string.strip(open(passwordFileName, 'r').readline())
-
-# get the unix login of user running this process
 unixLogin = os.environ['USER']
-
-# get the file we are logging
-fileToLog = os.environ['FILETOLOG']
-
-# get the file type we are logging
+workFile = os.environ['LOGWORKFILE']
+outputDir = os.environ['LOGOUTPUTDIR']
 fileType = os.environ['LOGFILETYPE']
-
-# if we don't have a valid file, exit(1)
-if not os.path.isfile(fileToLog):
-    sys.exit('%s is not a valid file' % fileToLog)
 
 # Initialize db.py DBMS parameters
 db.set_sqlLogin(user, password, server, database)
 
-# create a timestamp to log
-localtime = time.localtime(os.stat(fileToLog)[8])
+localtime = time.localtime(os.stat(workFile)[8])
 fileTimeStamp = '%s/%s/%s %s:%s' \
     % (localtime[1], localtime[2], localtime[0], localtime[3], localtime[4])
 
-# get the file size to log
-fileSize = os.stat(fileToLog)[6] / 1000
+fileSize = os.stat(workFile)[6] / 1000
 if fileSize == 0:
     fileSize = 1
 
 # log the file to RADAR
 db.sql('exec APP_logMirroredFile "%s", "%s", %s, "%s", "%s"' 
-    % (fileType, fileToLog, fileSize, fileTimeStamp, unixLogin), None)
+    % (fileType, os.path.join(outputDir, workFile), fileSize, fileTimeStamp, unixLogin), None)
+
+# only print out the files that were logged on the current date
+
+results = db.sql('select fileName, fileSize from APP_FilesMirrored ' + \
+	'where fileType = "%s" ' % (fileType) + \
+	'and convert(char(10), creation_date, 101) = "%s" ' % (cdate) + \
+	'order by _File_key', 'auto')
+for r in results:
+    print r['fileName'], `r['fileSize']`
 
 sys.exit(0)
 
